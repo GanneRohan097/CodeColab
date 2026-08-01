8623ed547da0b25055b1df28f295f4ebd0c1bc6f
GanneRohan097 <gannerohan@gmail.com>
2026-08-01 10:33:22 +0530

    Changes in ui

--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -19,8 +19,7 @@ const Room = () => {
         socket.current = new WebSocket("wss://codecolab-e7rb.onrender.com/");//ws://localhost:7000/
         socket.current.onopen = () => {
             console.log("Connected to server");
-
-            //socket.send("Hello server...");
+            //socket.send("Hello server..");
             //Now the browser will send the text "Hello Server" immediately after connecting.
             socket.current.send(
                 JSON.stringify({
